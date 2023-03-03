f788e991c6fb1892763b643294d155e7b38aedda
Claire Horvath <horvathclaire16@gmail.com>
2023-03-03 16:55:22 -0500

    final code

---
+++ sdlc.js
@@ -0,0 +1,34 @@
+// In this section you will answer some work through problems related to the Software Development Lifecycle
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
+*/
+
+// Implementation
+// Design
+// Maintain
+// Analysis of Requirements
+// Testing & Integration
+// Planning
+
+console.log("planning,", "analysis of requirements,", "design,", "implementation,", "testing and integration,", "maintain.");
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
+    The first one is done for you. 
+*/
+const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added.";
+const analysisOfRequirements = "Analyze and refine the already determined goal along with its necessary process'es' and requirements.";
+const design = "Clearly lay out all of the modules necessary to create the finished product and how it will connect to the data flow";
+const implementation = "This is when all of the code is written and the product is fully developed.";
+const testingAndIntegration = "The fully built product is tested and all issues are reported then fixed, then tested again until perfect.";
+const maintain = "System maintainance and updates will happen overtime to improve the product, user experience, and ensure that it is still running as anticipated.";
+
+console.log(planning);
+console.log(analysisOfRequirements);
+console.log(design);
+console.log(implementation);
+console.log(testingAndIntegration);
+console.log(maintain);
